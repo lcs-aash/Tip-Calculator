1ccbc3e4b923e950e767e68788a9c5e0cae042e5
Rori Ash <aash@lcs.on.ca>
2019-10-10 11:22:12 -0400

    Initial Commit

---
+++ Tip Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tip Calculator
+//
+//  Created by Ash, Aurora on 2019-10-10.
+//  Copyright © 2019 Rori Ash. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

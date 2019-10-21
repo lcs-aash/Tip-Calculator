7091c3f13655f2e8791e6a43b62397d6e5815624
Rori Ash <aash@lcs.on.ca>
2019-10-21 15:14:10 -0400

    Converted all values to double data type

--- Tip Calculator/ViewController.swift
+++ Tip Calculator/ViewController.swift
@@ -11,11 +11,16 @@ import UIKit
 class ViewController: UIViewController {
 
     // Mark: Properties (Outlets)(Constants)
-    var billAmount: UITextField!
-    var tipPercentage: UITextField!
-    var peopleCount: UITextField!
-    var totalTipInDollars: UILabel!
-    var tipPerPersonInDollars: UILabel!
+    @IBOutlet weak var billAmount: UITextField!
+    
+    @IBOutlet weak var tipPercentage: UITextField!
+    
+    @IBOutlet weak var peopleCount: UITextField!
+    
+    @IBOutlet weak var totalTipInDollars: UILabel!
+    
+    @IBOutlet weak var tipPerPersonInDollars: UILabel!
+    
 
 
     // Mark: Methods (functions)
@@ -27,12 +32,36 @@ class ViewController: UIViewController {
         
         
     }
-
-    // Get the user input
-     
     
     // Calculate the tip and report results
+    @IBAction func calculateTip(_ sender: Any) {
 
 
+    // Get the user input
+    let billAmountAsString = billAmount.text!
+    
+    let tipPercentageAsString = billAmount.text!
+        
+    let peopleCountAsString = peopleCount.text!
+        
+    
+        // Convert all data values to double data type
+        let billAmountAsDouble = Double(billAmountAsString)!
+        
+        let tipPercentageAsDouble = Double (tipPercentageAsString)! / 100
+        
+        let peopleCountAsDouble = Double (peopleCountAsString)!
+        
+        // Calculate tip in dollars
+        let totalTip = billAmountAsDouble * tipPercentageAsDouble
+        
+        // Calculate tip per person
+        let tipPerPerson = totalTip / peopleCountAsDouble
+        
+        // Send the results to the view
+        totalTipInDollars.text = String(totalTip)
+        
+        tipPerPersonInDollars.text = String(tipPerPerson)
 }
 
+}
